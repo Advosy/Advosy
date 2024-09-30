88b82106294b5f4b3197f9d03431470cc1417af6
Advosy <scott@advosyenergy.com>
2024-09-30 16:18:38 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,4 +3,46 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/app.js',
-  '/
+  '/styles.css',
+  '/app-icon-192.png', // Add your app icons here
+  '/icon-512.png'
+];
+
+// Install Service Worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch the cached resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached response if found, else fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate the service worker and remove old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
